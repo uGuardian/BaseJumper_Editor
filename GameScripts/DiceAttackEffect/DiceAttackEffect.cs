d166429723cb0de546cea367820d18199ebab7f8
uGuardian <uGuardian@users.noreply.github.com>
2024-02-27 21:39:55 -0700

    Removed more errors

--- GameScripts/DiceAttackEffect/DiceAttackEffect.cs
+++ GameScripts/DiceAttackEffect/DiceAttackEffect.cs
@@ -48,73 +48,67 @@ namespace Battle.DiceAttackEffect
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public virtual void Initialize(BattleUnitView self, BattleUnitView target, float destroyTime)
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public virtual void SetLayer(string layerName)
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public virtual void SetScale(float scaleFactor)
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public virtual void SetDestroyTime(float destroyTime)
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		protected virtual void Awake()
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		protected virtual void Start()
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		protected virtual void Update()
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		protected virtual void OnDestroy()
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public virtual void ResetLocalTransform(Transform t)
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public virtual void SetAutoDestruction(GameObject go, float time, bool destroyWhenDisable)
 		{
-			
+
 		}
 
 		[MethodImpl(MethodImplOptions.NoInlining)]
 		public virtual void AddFilter(Type filter)
 		{
-			
-		}
 
-		[MethodImpl(MethodImplOptions.NoInlining)]
-		public DiceAttackEffect()
-		{
-			throw null;
 		}
 	}
 }
